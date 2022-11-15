ab0bf0721ec3a6c8cfccc3529050235ac8242340
waiphyoaung <rubyland.myanmar.mg@gmail.com>
2022-11-15 12:19:11 +0630

    flutter web server run from difference folder

--- auto_run.js
+++ auto_run.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const subProcess = require('child_process')
 const app = express();
-const command = '../../besu-21.10.6/bin/./besu --data-path=../../besu_server/node1/data --genesis-file=../../besu_server/genesis.json --network-id 123 --rpc-http-enabled --rpc-http-api=ETH,NET,CLIQUE --host-allowlist="*" --rpc-http-cors-origins="all" --rpc-http-host=0.0.0.0 --min-gas-price=0';
+const command = 'cd ../GBNX_ADMIN && flutter run -d web-server --web-port 1337 --web-hostname 0.0.0.0 --release --web-renderer html';
 app.listen(4000, () => {
 	subProcess.exec(command, (err, stdout, stderr) => {
 		if (err) {
